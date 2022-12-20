941891d6ca8e700d031b4953b0b2cc6d8469d077
Rin-t <rin.t.4921@gmail.com>
2022-12-20 23:25:16 +0900

    ViewControllerの処理を作成

--- LoginRxSwiftSample/ViewController.swift
+++ LoginRxSwiftSample/ViewController.swift
@@ -6,12 +6,70 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmationPasswordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var loginStatusLabel: UILabel!
+    private let dispodeBag = DisposeBag()
+    private let viewModel = ViewModel(model: LoginModel())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        emailTextField.rx.text
+            .map{ $0 ?? "" }
+            .subscribe(onNext: { [weak self] text in
+                print(text)
+                guard let strongSelf = self else { return }
+                strongSelf.viewModel.input.emailTextField.accept(text)
+            })
+            .disposed(by: dispodeBag)
+        
+        passwordTextField.rx.text
+            .map{ $0 ?? "" }
+            .subscribe(onNext: { [weak self] text in
+                guard let strongSelf = self else { return }
+                strongSelf.viewModel.input.passwordTextField.accept(text)
+            })
+            .disposed(by: dispodeBag)
+        
+        confirmationPasswordTextField.rx.text
+            .map{ $0 ?? "" }
+            .subscribe(onNext: { [weak self] text in
+                guard let strongSelf = self else { return }
+                strongSelf.viewModel.input.confirmationPasswordTextField.accept(text)
+            })
+            .disposed(by: dispodeBag)
+        
+        loginButton.rx.tap
+            .subscribe { [weak self] _ in
+                guard let strongSelf = self else { return }
+                strongSelf.viewModel.input.tappedRegisterButton.accept(())
+            }
+            .disposed(by: dispodeBag)
+        
+        viewModel.output.isValidate
+            .subscribe(onNext: { [weak self] isValidate in
+                guard let strongSelf = self else { return }
+                strongSelf.loginButton.isHidden = !isValidate
+            })
+            .disposed(by: dispodeBag)
+        
+        viewModel.output.showAlert
+            .subscribe(onNext: { [weak self] message in
+                guard let strongSelf = self else { return }
+                strongSelf.loginStatusLabel.text = message
+            })
+            .disposed(by: dispodeBag)
+            
     }
 
 
