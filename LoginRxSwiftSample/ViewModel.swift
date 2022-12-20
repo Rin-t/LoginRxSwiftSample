a09899596b32a068c5e9fb7f7395feb011a41fcc
Rin-t <rin.t.4921@gmail.com>
2022-12-20 21:41:38 +0900

    ViewModelの処理を作成

--- LoginRxSwiftSample/ViewModel.swift
+++ LoginRxSwiftSample/ViewModel.swift
@@ -13,11 +13,12 @@ protocol ViewModelInput {
     var passwordTextField: PublishRelay<String> { get }
     var confirmationPasswordTextField: PublishRelay<String> { get }
     var emailTextField: PublishRelay<String> { get }
-    var tappedButton: PublishRelay<Void> { get }
+    var tappedRegisterButton: PublishRelay<Void> { get }
 }
 
 protocol ViewModelOutput {
-    var isValidate: BehaviorRelay<Bool> { get }
+    var isValidate: Observable<Bool> { get }
+    var showAlert: Observable<String> { get }
 }
 
 protocol ViewModelType {
@@ -26,22 +27,66 @@ protocol ViewModelType {
 }
 
 final class ViewModel: ViewModelInput, ViewModelOutput {
+    
+    
 
     //MARK: - inputs
     var passwordTextField = PublishRelay<String>()
     var confirmationPasswordTextField = PublishRelay<String>()
     var emailTextField = PublishRelay<String>()
-    var tappedButton = PublishRelay<Void>()
+    var tappedRegisterButton = PublishRelay<Void>()
+    
 
     //MARK: - outputs
-    let isValidate: BehaviorRelay<Bool>
-
+    private var isValidateRelay = BehaviorRelay<Bool>(value: false)
+    private var alertMessage = PublishRelay<String>()
+    
+    var isValidate: Observable<Bool> {
+        return isValidateRelay.asObservable()
+    }
+    var showAlert: Observable<String> {
+        return alertMessage.asObservable()
+    }
+    
+    private let loginModel: LoginModelProtocol
     private let disposeBag = DisposeBag()
 
-    init() {
+    init(model: LoginModelProtocol) {
+        loginModel = model
+        
+        // ログインボタンの活性、非活性を判定。判定処理はModel側に持たせるべきか
+        Observable
+            .combineLatest(passwordTextField.asObservable(),
+                           confirmationPasswordTextField.asObservable(),
+                           emailTextField.asObservable())
+            .map { passWord, confirmPassWord, email in
+                passWord.isPasswordValidate()
+                && passWord == confirmPassWord
+                && email.isEmailValidate()
+            }
+            .bind(to: isValidateRelay)
+            .disposed(by: disposeBag)
+      
+        tappedRegisterButton
+            .subscribe { [weak self] _ in
+                guard let strongSelf = self else { return }
+                strongSelf.loginModel.requestLogin()
+                    .subscribe { event in
+                        switch event {
+                        case .success:
+                            let message = "ログイン成功"
+                            strongSelf.alertMessage.accept(message)
+                        case .failure(let error):
+                            let error = error as! LoginError
+                            strongSelf.alertMessage.accept(error.message)
+                        }
+                    }
+                    .disposed(by: strongSelf.disposeBag)
+    
+            }
+            .disposed(by: disposeBag)
 
     }
-
 }
 
 extension ViewModel: ViewModelType {
@@ -51,7 +96,7 @@ extension ViewModel: ViewModelType {
 
 }
 
-extension String {
+private extension String {
 
     func isEmailValidate() -> Bool {
         contains("hotate")
@@ -62,3 +107,11 @@ extension String {
     }
 }
 
+private extension LoginError {
+    
+    var message: String {
+        switch self {
+        case .connectionError: return "ネットワークエラー"
+        }
+    }
+}
